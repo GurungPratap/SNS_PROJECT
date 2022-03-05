565c98f1d2b8dc6866e2e391fc6947cf5af683c5
PratapGurung <prlg.dev52@gmail.com>
2022-03-05 15:51:22 -0600

    initial project setupt commit

---
+++ src/main/java/com/sns/project/sns_core/SnsApplication.java
@@ -0,0 +1,13 @@
+package com.sns.project.sns_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsApplication.class, args);
+    }
+
+}
