a6a0aa0e9b314b57278131ee519efb2ba68ba220
PratapGurung <prlg.dev52@gmail.com>
2022-03-13 17:50:12 -0500

    added config server dependency

--- src/main/java/com/sns/project/sns_core/SnsApplication.java
+++ src/main/java/com/sns/project/sns_core/SnsApplication.java
@@ -2,10 +2,11 @@ package com.sns.project.sns_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class SnsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SnsApplication.class, args);
     }
--- src/main/java/com/sns/project/sns_core/controller/UserPostController.java
+++ src/main/java/com/sns/project/sns_core/controller/UserPostController.java
@@ -3,6 +3,8 @@ package com.sns.project.sns_core.controller;
 import com.sns.project.sns_core.model.UserPost;
 import com.sns.project.sns_core.service.UserPostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,4 +29,5 @@ public class UserPostController {
         List<UserPost> result = userPostService.getPostsByUserName(username);
         return result;
     }
+
 }
